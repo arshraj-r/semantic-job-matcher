f2c7a7f95a63ec423c4c8f0ea1945bedeecd9aa3
arshraj-r <arshraj.randhawa@gmail.com>
2025-04-28 12:34:24 +0530

    updates

--- daily_scrape.py
+++ daily_scrape.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from app.linkedin_scraper import   scrape_jobs_for_keywords
 from app.resume_parser import extract_text_from_resume  
+from app.similarity_score import  similarity_score_calculator
 
 location="Bengaluru"
 keywords=["LLM", "NLP", "Gen AI", "Data Scientist"]
@@ -12,5 +13,4 @@ jobs=pd.read_csv("daily_jobs/jobs_df.csv")
 jobs=jobs[:10]
 print(f"running code for: {jobs.shape[0]}")
 for i in range(jobs.shape[0]):
-    jobs["link"].values[i]
-    
+    link=jobs["link"].values[i]
