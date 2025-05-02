91d42bd5eda901b89f4e1c424ecf373b1c9be338
arshraj-r <arshraj.randhawa@gmail.com>
2025-05-02 11:30:25 +0530

    updates

--- daily_scrape.py
+++ daily_scrape.py
@@ -2,21 +2,12 @@ import pandas as pd
 from app.linkedin_scraper import   scrape_jobs_for_keywords
 from app.resume_parser import extract_text_from_resume  
 from app.similarity_score import  similarity_score_calculator
-
-location="Bengaluru"
-keywords=["LLM", "NLP", "Gen AI", "Data Scientist"]
-scroll_page=2
-# jobs = scrape_jobs_for_keywords(job_keywords=keywords, location=location, scroll_page=scroll_page)
-# jobs.to_csv("daily_jobs/jobs_df.csv",index=False)
-
-# jobs=pd.read_csv("daily_jobs/jobs_df.csv")
-# jobs=jobs[:10]
-# print(f"running code for: {jobs.shape[0]}")
-# for i in range(jobs.shape[0]):
-#     link=jobs["link"].values[i]
-
-
 from app.naukari_scrapper import scrape_naukari_jobs
+
+
+keywords=["LLM", "NLP", "Gen AI", "Data Scientist"]
+jobs = scrape_jobs_for_keywords(job_keywords=keywords, location="Bengaluru", scroll_page=2)
+jobs.to_csv("daily_jobs/linkedin_jobs_df.csv",index=False)
 
 naukari_jobs_df=scrape_naukari_jobs(keywords, location = "bengaluru", experience = 5)
 naukari_jobs_df.to_csv("daily_jobs/naukari_jobs_df.csv",index=False)
